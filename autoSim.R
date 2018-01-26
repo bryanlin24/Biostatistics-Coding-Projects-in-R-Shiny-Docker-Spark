f6ddf73eaaf4e1e2fb6d37cd2b7d595a2e948908
Bryan Lin <bryanlin24@g.ucla.edu>
2018-01-26 03:22:13 +0000

    work on autoSim.R. Text files are not printing out anything

--- autoSim.R
+++ autoSim.R
@@ -2,25 +2,21 @@
 rep = 50
 seed = 280
 distr = c("gaussian", "t1", "t5")
-
-
 nVals = seq(100, 500, by=100)
+
+
 for (n in nVals) {
   for (dist in distr){
  
-  oFile = paste(dist, "n", n, ".txt", sep="")
+  oFile = paste(dist,"n", n, ".txt", sep=",")
   
   #modify arg
-  arg = paste("n=", n, sep="", "seed=", seed, sep="", "distr=",dist, sep="", "rep=", rep, sep="")
-  arg = paste("n=", n, sep="")
-  
-  #no change
+  arg = paste(paste("n=", n, sep=","), paste("seed=", seed, sep=","), paste("distr=",dist, sep=","), paste("rep=", rep, sep=""))
+
   sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
   
-  #no change
   system(sysCall)
   
-  #no change
   print(paste("sysCall=", sysCall, sep=""))
   }
 }
