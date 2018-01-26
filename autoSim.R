326d1f46374799300d1c8f5b2361576381feaca1
Bryan Lin <bryanlin24@g.ucla.edu>
2018-01-26 02:58:54 +0000

    start working on autoSim.R

--- autoSim.R
+++ autoSim.R
@@ -1,12 +1,28 @@
 # autoSim.R
+rep = 50
+seed = 280
+distr = c("gaussian", "t1", "t5")
+
 
 nVals = seq(100, 500, by=100)
 for (n in nVals) {
-  oFile = paste("n", n, ".txt", sep="")
+  for (dist in distr){
+ 
+  oFile = paste(dist, "n", n, ".txt", sep="")
+  
+  #modify arg
+  arg = paste("n=", n, sep="", "seed=", seed, sep="", )
   arg = paste("n=", n, sep="")
+  
+  #no change
   sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
+  
+  #no change
   system(sysCall)
+  
+  #no change
   print(paste("sysCall=", sysCall, sep=""))
+  }
 }
 
 
