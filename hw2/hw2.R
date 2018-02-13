e08a90e229a9ba78f6e39faf3556aa09e9d99a75
Bryan Lin <bryanlin24@g.ucla.edu>
2018-02-13 01:45:38 +0000

    Finished exercise 7.4.1 (worked on this exercise on a laptop in the library because I did not have access to my local machine).

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -11,81 +11,96 @@ library(tidyr)
 
 View(diamonds)
 
+#Question 7.3.4
+# Explore the distribution of each of the x, y, and z variables in 
+# diamonds. What do you learn? Think about a diamond and how you might 
+# decide which dimension is the length, width, and depth
+
+
 #Define variables
+
 x <- diamonds$x
 y <- diamonds$y
 z <- diamonds$z
+
+
+x <- cbind(x, y, z)
+View(x)
+
 
 hist(x, main = "Hist of x in Diamonds")
 hist(y, main = "Hist of y in Diamonds")
 hist(z, main = "Hist of z in Diamonds")
 
 
-
-#Histograms
-xyz <- rbind(x, y, z)
-
-typeof(xyz)
-xyz<-as.data.frame((xyz))
-
-ggplot(data = xyz, aes(value))
-ggplot(vegLengths, aes(length, fill = veg)) + geom_density(alpha = 0.2)
-
-
-
-ggplot(data = diamonds) + geom_histogram(aes(x=x), fill="lightblue")
-+ geom_histogram(aes(x=y),  fill="darkblue")
-
-ggplot() + 
-  geom_histogram(aes(x=df1$Age, y=..density..),  fill="lightblue") + geom_histogram(aes(x=df2$Age, y=..density..),  fill="darkblue")
-
-
-
-
-#Histogram of x
-
-ggplot(data = diamonds, aes(x)) + geom_histogram()
-ggplot(data = diamonds, aes(y)) + geom_histogram()
-
-
-ggplot(data = diamonds, aes(x)) + 
-  geom_histogram(breaks = seq(0, 60, by = 1) col="red", 
-                 fill="green", 
-                 alpha = .5) + 
-  labs(title="Histogram for x") +
-  labs(x="x", y="Count")
-
-#Distribution has very small tails
-#Most data clustered toward the middle
-#Has a rough bell shape
-
-
-#Histogram of y
-ggplot(data = diamonds, aes(y)) + 
-  geom_histogram(col="red", 
-                 fill="green", 
-                 alpha = .5) + 
-  labs(title="Histogram for y") +
-  labs(x="y", y="Count")
-
-#Distribution is highly skewed right
-
-
-#Histogram of z
-ggplot(data = diamonds, aes(z)) + 
-  geom_histogram(col="red", 
-                 fill="green", 
-                 alpha = .5) + 
-  labs(title="Histogram for z") +
-  labs(x="z", y="Count")
-
-#Distribution is highly skewed right
-
-
-
-
-
+max(z)
+max(y)
 diamonds %>%
   gather(key = dist, vals, x, y, z) %>%
   ggplot(aes(vals, colour = dist)) +
   geom_freqpoly(bins = 100)
+
+
+price <- diamonds$price
+
+
+carat <- diamonds$carat
+
+diamonds %>%
+  subset(diamonds, carat %in% c(0.99,1))
+
+diamonds %>%
+  filter(diamonds, carat %in% c(0.99,1))
+
+filter_carat <- diamonds %>%
+  filter(carat >= 0.99 & carat <= 1)
+
+max(filter_carat$carat)
+
+View(diamonds)
+
+
+
+
+
+
+
+
+#Define carat variable
+carat <- diamonds$carat
+
+filter_carat <- diamonds %>%
+  filter(carat >= 0.99 & carat <= 1)
+
+#Check if code worked. Min and max should be between 0.99 and 1 inclusive
+min(filter_carat$carat)
+max(filter_carat$carat)
+
+#Define filtered carat variable
+filtered_carat <- filter_carat$carat
+
+
+#Check if code worked. Min and max should be between 0.99 and 1
+min(filtered_carat)
+max(filtered_carat)
+
+count(filtered_carat)
+
+
+diamonds %>%
+  ggplot(aes(y)) +
+  geom_histogram() +
+  coord_cartesian(ylim = c(0, 50))
+# Note how xlim deleted the observations at 0.
+
+
+diamonds %>%
+  ggplot(aes(x)) +
+  geom_histogram() +
+  coord_cartesian(ylim = c(0, 50))
+# Note how xlim deleted the observations at 0.
+
+ggplot(diamonds) + 
+  geom_histogram(mapping = aes(x = price)) +
+  xlim(100, 5000) +
+  ylim(0, 3000)
