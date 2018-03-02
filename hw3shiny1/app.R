2f08e6ecad27b8f39858bed215d275157b6085ae
Bryan Lin <bryanlin24@g.ucla.edu>
2018-03-02 13:33:54 -0800

    Continue working on hw3 q1 highest earners

--- hw3shiny1/app.R
+++ hw3shiny1/app.R
@@ -22,7 +22,7 @@ ui <- fluidPage(
                )
              )
     )
-  ),
+    ),
   
   #Make panel for Who earned the most?
   tabPanel("Top Paid LA Employees", uiOutput("q3"), fluid = TRUE,
@@ -30,19 +30,18 @@ ui <- fluidPage(
            sidebarLayout(
              sidebarPanel(
                numericInput(inputId = "rowq3", label = "How many rows?",
-                            value = 10, min = 5, max = count(earn), step = 10),
-               selectInput(inputid = "useryearq3", label = "Year", choices = c(distinct(earn, Year)), selected = 2017),
-               width=2
+                            value = 10, min = 5, max = count(earn), step = 5),
+               selectInput(inputId = "useryearq3", label = "Year",
+                           choices = c(distinct(earn, Year)), selected = 2017),
+               width = 2
+             ),
+             mainPanel(
+               tableOutput("tableq3")
              )
            )
-  
-  
-  
-  
-  
-  
-  
+  )
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -50,7 +49,15 @@ server <- function(input, output) {
   output$totalpaybarplot <- renderPlot({
     ggplot(data = totalpay, aes(x=Year, y=Amount, fill=Type)) +
       geom_bar(stat="identity", position=position_dodge())
-   })
+  })
+  
+  output$tableq3 <- renderTable({
+    earn %>%
+      group_by(Year) %>%
+      filter(Year == input$useryearq3) %>%
+      arrange(desc(Total_Payments)) %>%
+      head(input$rowq3)
+  })
 }
 
 # Run the application 
